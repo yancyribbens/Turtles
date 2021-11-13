c5be29743ab640013c98e6cad457c1124b645914
Yancy <email@yancy.lol>
2021-11-13 17:24:53 +0100

    Add ErrA and ErrB

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
-pub enum OriginalErr {
+pub enum ErrB {
+    Turtles
+}
+
+pub enum ErrA {
     Turtles
 }
 
 pub enum AppErr {
-    Oops(crate::OriginalErr)
+    OopsA(crate::ErrA),
+    OopsB(crate::ErrB)
 }
 
-impl From<crate::OriginalErr> for AppErr {
-    fn from(error: crate::OriginalErr) -> Self {
-        AppErr::Oops(error)
+impl From<crate::ErrA> for AppErr {
+    fn from(error: crate::ErrA) -> Self {
+        AppErr::OopsA(error)
+    }
+}
+
+impl From<crate::ErrB> for AppErr {
+    fn from(error: crate::ErrB) -> Self {
+        AppErr::OopsB(error)
     }
 }
 
 fn danger_will_robinson() -> Result<i32, AppErr> {
-    let e:AppErr = OriginalErr::Turtles.into();
+    let e:AppErr = ErrA::Turtles.into();
+    let e:AppErr = ErrB::Turtles.into();
+
     Err(e)
 }
 
